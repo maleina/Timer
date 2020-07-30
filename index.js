fde013907ef04f18a0bba99b61c33b3ca8ae711d
Maleina Bidek <maleina@ucla.edu>
2020-07-30 13:51:03 -0400

    feat: Add initial start and pause functionality for timer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hi there!")
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    tick = () => {
+        console.log('tick');
+    };
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
